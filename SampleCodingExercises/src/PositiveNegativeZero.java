36934002b602ce883b5171f68118ab0d2140b3e0
Moose17 <tom@moose17.dev>
2023-05-03 09:57:54 +0100

    create method checkNumber to validate argument as a positive or negative number and edge cased 0 as 'zero'

---
+++ SampleCodingExercises/src/PositiveNegativeZero.java
@@ -0,0 +1,12 @@
+public class PositiveNegativeZero {
+    public static void checkNumber (int number) {
+        if(number == 0){
+            System.out.println("zero");
+        } else if (number > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+
+    }
+}
